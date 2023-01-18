5cc7629edee8ba76fabd4145ff3ec4329ae65839
hareldamti <94783416+hareldamti@users.noreply.github.com>
2023-01-18 15:09:45 +0200

    date add for activityee

--- action.py
+++ action.py
@@ -17,8 +17,7 @@ def main(args : list[str]):
     inputfilename : str = args[1]
     with open(inputfilename) as inputfile:
         for line in inputfile:
-            splittedline : list[str] = line.strip().split(", ")
-            #TODO: apply the action (and insert to the table) if possible
+            splittedline : list[str] = line.strip("\n").split(", ")
             buyProduct(splittedline) if(int(splittedline[1]) > 0) else sellProduct(splittedline)
 if __name__ == '__main__':
     main(sys.argv)
