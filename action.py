d1b8f2df4c37a48af78b19233b6a4f9750f05389
asimai60 <80712898+asimai60@users.noreply.github.com>
2023-01-21 19:22:18 +0200

    Update action.py
    the amount to sell is already negative we need to add it to the total

--- action.py
+++ action.py
@@ -11,7 +11,7 @@ def sellProduct(splittedline : list[str]):
     productRecord = repo.products.find(id = splittedline[0])
     if(abs(int(splittedline[1])) <= int(productRecord[0].quantity)):
         repo.activities.insert(Activitie(*splittedline))
-        repo.products.update({"quantity": str(int(productRecord[0].quantity) - int(splittedline[1]))},{"id" : splittedline[0]})
+        repo.products.update({"quantity": str(int(productRecord[0].quantity) + int(splittedline[1]))},{"id" : splittedline[0]})
 
 def main(args : list[str]):
     inputfilename : str = args[1]
